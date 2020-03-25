51de162dd12a552dbba221b51e8908db64809d95
Bruno Mahiet <brunomahiet79@gmail.com>
2020-03-25 12:11:04 +0100

    initialisation de l'index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tutoGit</title>
+</head>
+<body>
+    <p>Tutoriel sur git et github</p>
+</body>
+</html>
